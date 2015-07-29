bc67b4c17f0d43976e4220fe3302a07273d39958
RyanSmith <ryansmith13@outlook.com>
2015-07-29 15:30:31 +0100

    Created directory structure and added basic config

---
+++ conf/config.php
@@ -0,0 +1,23 @@
+<?php
+
+return array
+(
+    //App Config
+    'app_name'      => 'Openhaus',
+    'environment'   => 'test',          //Change to production on deploy
+    'domain'        => 'openhaus.ca',
+
+    //Database
+    'db_location'   => 'localhost',
+    'db_name'       => 'openhaus',
+    'db_username'   => 'root',
+    'db_password'   => 'r160689s',
+
+    //Email Settings
+    'email_system'  => 'support@openhaus.com',
+    'email_support'  => 'support@openhaus.com',
+    'smtp_host'  => 'smtp.gmail.com',
+    'smtp_user'  => 'rydosmith2@gmail.com',
+    'smtp_password'  => 'R160689s'
+
+);
